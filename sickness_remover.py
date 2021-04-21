95341046d6820c2299d2ffba685c8cc4fb7f0281
CompuGenius Programs <contact@cgprograms.com>
2021-04-21 12:58:21 -0400

    Update sickness_remover.py

--- sickness_remover.py
+++ sickness_remover.py
@@ -2,10 +2,8 @@ from earth import humans
 from earth import sicknesses
 
 for human in range(len(humans)):
-    if human.is_sick:
-      human.is_sick = False
-    if not human.has_antibodies:
-      human.has_antibodies = True
+    human.is_sick = False
+    human.has_antibodies = True
       
 humans.need_to_mask = False
 humans.need_to_quarantine = False
