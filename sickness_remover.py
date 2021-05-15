76ec432f6f92ba887d51e418c011af2762394a71
MysteriousK69 <63832146+MysteriousK69@users.noreply.github.com>
2021-05-15 14:40:49 +0530

    preventing karens, anti vaxxers, anti maskers

--- sickness_remover.py
+++ sickness_remover.py
@@ -4,12 +4,14 @@ from earth import sicknesses
 for human in humans:
     human.is_sick = False
     human.has_antibodies = True
+    if human.is_karen or human.is_anti_vaxxer or human.is_anti_masker:
+        humans.pop(humans.index(human))
 
 for sickness in sicknesses:
     sickness.active = False
     sickness.has_cure = True
-    
 
-      
-humans.need_to_mask = False
+
+humans.need_to_mask = True
+humans.need_to_get_vaccinated = True
 humans.need_to_quarantine = False
